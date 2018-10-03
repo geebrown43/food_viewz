291d322d2c76bc888214aff2eda7412c1bf341d4
Gavin Brown <Scareb15@gmail.com>
2018-10-02 20:35:49 -0600

    added landing stack

--- src/navigation/stacks/landingStack.js
+++ src/navigation/stacks/landingStack.js
@@ -1,11 +1,25 @@
 import React from "react"
 import {createStackNavigator} from "react-navigation"
 import Landing from "../../screens/Landing"
+import Login from "../../screens/Landing/Login"
+import Signup from "../../screens/Landing/Sign-Up"
 
 const LandingStack = createStackNavigator({
     Landing: {
         screen: Landing,
         navigationOptions:{
+            header: null
+        }
+    },
+    Login: {
+        screen: Login,
+        navigationOptions: {
+            header: null
+        }
+    },
+    Signup: {
+        screen: Signup,
+        navigationOptions: {
             header: null
         }
     } 
--- src/screens/Landing/LandingScreen.js
+++ src/screens/Landing/LandingScreen.js
@@ -4,19 +4,20 @@ import {View, Text, StyleSheet, Image, TouchableOpacity, Dimensions} from "react
 const {width, height} = Dimensions.get("screen")
 export default class LandingScreen extends Component {
     render(){
+        const {navigateLogin, navigateSignup} = this.props
         return(
             <View style={styles.container}>
                 <Image source={require("../../../ios/assets/Icon_hammburger.png")} style={styles.image}/>
-                <View>
-                    <TouchableOpacity>
-                        <View style={styles.buttonContainer}>
-                            <Text>Sign-up</Text>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity onPress={navigateSignup}>
+                        <View style={styles.button}>
+                            <Text style={styles.buttonText}>Sign-up</Text>
                         </View>
                     </TouchableOpacity>
-                    <View />
-                    <TouchableOpacity>
-                        <View style={styles.buttonContainer}>
-                            <Text>Login</Text>
+                    <View style={styles.line}/>
+                    <TouchableOpacity onPress={navigateLogin}>
+                        <View style={styles.button}>
+                            <Text style={styles.buttonText}>Login</Text>
                         </View>
                     </TouchableOpacity>
                 </View>
@@ -29,21 +30,33 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#5F1FD6",
-        alignItems: "center"
+        alignItems: "center",
+        justifyContent: "space-around"
     },
     image: {
         width: width / 1.7,
         height: width / 1.7
     },
     line: {
-        color: "white",
+        backgroundColor: "white",
         width: width,
         height: 4
     },
-    buttonContainer: {
+    button: {
         backgroundColor: "white",
         borderRadius: 4,
-        width: width / 1.7,
-        height: height / 16
+        width: width / 1.5,
+        height: height / 16,
+        alignItems: "center",
+        justifyContent:"center",
+        margin: 10
+    },
+    buttonText: {
+        fontFamily: "HelveticaNeue-CondensedBold",
+        fontSize: 24,
+        color: "#5F1FD6"
+    },
+    buttonContainer: {
+        alignItems: "center"
     }
 })
