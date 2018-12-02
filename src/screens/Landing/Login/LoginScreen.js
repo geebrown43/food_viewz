474663a5170f2022c65303f1ec2ae6e3164a7750
Gavin Brown <scareb15@gmail.com>
2018-12-02 00:49:52 -0700

    added firebase to app

--- src/screens/Landing/Login/LoginScreen.js
+++ src/screens/Landing/Login/LoginScreen.js
@@ -1,94 +1,122 @@
-import React, {Component} from "react"
-import {View, TextInput, StyleSheet, TouchableOpacity, Image, Dimensions, Text} from "react-native"
+import React, { Component } from "react";
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Dimensions,
+  Text
+} from "react-native";
 
-const {width, height} = Dimensions.get("screen")
+const { width, height } = Dimensions.get("screen");
 export default class LoginScreen extends Component {
-    render(){
-        const {navigate} = this.props
-        return(
-            <View style={styles.container}>
-                <Image source={require("../../../../ios/assets/Icon_hammburger.png")} style={styles.image}/>
-                <View style={styles.topContainer}>
-                    <View style={styles.inputContainer}>
-                        <TextInput style={styles.textInput} />
-                        <TextInput style={styles.textInput} />
-                    </View>
-                    <TouchableOpacity onPress={() => navigate("Home")}>
-                    <View style={styles.button}>
-                            <Text style={styles.buttonText}>Login</Text>
-                    </View>
-                    </TouchableOpacity>    
-                </View>
-                <View>
-                    <View style={styles.line} />
-                    <View style={{flexDirection: "row", justifyContent:"center", marginTop: 20}}>
-                    <Text style={styles.fineText}>Don't have an account ?</Text>
-                    <TouchableOpacity onPress={() => this.navigate("Signup")}>
-                        <Text style={styles.boldText}> Signup</Text>
-                    </TouchableOpacity>
-                    </View>
-                </View>
+  render() {
+    const { navigate, onChangeText, email, password, login } = this.props;
+    return (
+      <View style={styles.container}>
+        <Image
+          source={require("../../../../ios/assets/Icon_hammburger.png")}
+          style={styles.image}
+        />
+        <View style={styles.topContainer}>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.textInput}
+              onChangeText={email => onChangeText({email})}
+            />
+            <TextInput
+              style={styles.textInput}
+              onChangeText={password => onChangeText({password})}
+            />
+          </View>
+          <TouchableOpacity
+            onPress={() => {
+              login();
+              navigate("Home");
+            }}
+          >
+            <View style={styles.button}>
+              <Text style={styles.buttonText}>Login</Text>
             </View>
-        )
-    }
+          </TouchableOpacity>
+        </View>
+        <View>
+          <View style={styles.line} />
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              marginTop: 20
+            }}
+          >
+            <Text style={styles.fineText}>Don't have an account ?</Text>
+            <TouchableOpacity onPress={() => this.navigate("Signup")}>
+              <Text style={styles.boldText}> Signup</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#5F1FD6",
-        alignItems: "center",
-        justifyContent: "space-around"
-    },
-    image: {
-        width: width / 2.2,
-        height: width / 2.2
-    },
-    inputContainer: {
-        width: width / 1.15,
+  container: {
+    flex: 1,
+    backgroundColor: "#5F1FD6",
+    alignItems: "center",
+    justifyContent: "space-around"
+  },
+  image: {
+    width: width / 2.2,
+    height: width / 2.2
+  },
+  inputContainer: {
+    width: width / 1.15,
     height: height / 5,
     backgroundColor: "white",
     borderRadius: 4,
     margin: 5
-    },
-    button: {
-        backgroundColor: "white",
-        borderRadius: 4,
-        width: width / 1.3,
-        height: height / 16,
-        alignItems: "center",
-        justifyContent: "center",
-        margin: 10
-      },
-      topContainer: {
-        alignItems: "center"
-      },
-      line: {
-        backgroundColor: "white",
-        width: width ,
-        height: 3
-      },
-      buttonText: {
-        fontSize: 24,
-        fontFamily: "HelveticaNeue-CondensedBold",
-        color: "#5F1FD6"
-      },
-      fineText: {
-        color: "white",
-        fontFamily: "HelveticaNeue-Light",
-        fontSize: 14
-      },
-      boldText: {
-        color: "white",
-        fontFamily: "HelveticaNeue-Bold",
-        fontSize: 14
-      },
-      textInput: {
-        borderBottomWidth: 1,
-        borderBottomColor: "#D8D8D8",
-        flex: 1,
-        fontSize: 18,
-        fontFamily: "HelveticaNeue-Thin",
-        paddingLeft: 15
-      },
-})
+  },
+  button: {
+    backgroundColor: "white",
+    borderRadius: 4,
+    width: width / 1.3,
+    height: height / 16,
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 10
+  },
+  topContainer: {
+    alignItems: "center"
+  },
+  line: {
+    backgroundColor: "white",
+    width: width,
+    height: 3
+  },
+  buttonText: {
+    fontSize: 24,
+    fontFamily: "HelveticaNeue-CondensedBold",
+    color: "#5F1FD6"
+  },
+  fineText: {
+    color: "white",
+    fontFamily: "HelveticaNeue-Light",
+    fontSize: 14
+  },
+  boldText: {
+    color: "white",
+    fontFamily: "HelveticaNeue-Bold",
+    fontSize: 14
+  },
+  textInput: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#D8D8D8",
+    flex: 1,
+    fontSize: 18,
+    fontFamily: "HelveticaNeue-Thin",
+    paddingLeft: 15
+  }
+});
--- src/screens/Landing/Login/index.js
+++ src/screens/Landing/Login/index.js
@@ -1,17 +1,26 @@
-import React, {Component} from "react"
-import LoginScreen from "./LoginScreen"
+import React, { Component } from "react";
+import firebase from "react-native-firebase";
+import LoginScreen from "./LoginScreen";
 
 class Login extends Component {
-    navigate = (screen) => {
-        const {navigation} = this.props
-        navigation.navigate(screen)
-    }
-    render(){
-
-        return(
-            <LoginScreen navigate={this.navigate}/>
-        )
-    }
+  state = {
+    email: "",
+    password: ""
+  };
+  navigate = screen => {
+    const { navigation } = this.props;
+    navigation.navigate(screen);
+  };
+  login = () => {
+    const { email, password } = this.state;
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(err => console.log(err));
+  };
+  render() {
+    return <LoginScreen navigate={this.navigate} login={this.login} onChangeText={this.setState.bind(this)} {...this.state}/>;
+  }
 }
 
-export default Login
+export default Login;
